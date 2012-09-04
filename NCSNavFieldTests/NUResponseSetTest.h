b066481337e4a14ba63e9bec0ffd4472cf71d32f
John Dzak <john.dzak@gmail.com>
2012-09-04 11:33:56 -0500

    Add pId to response set to data model. #2420

---
+++ NCSNavFieldTests/NUResponseSetTest.h
@@ -0,0 +1,17 @@
+//
+//  NUResponseSetTest.h
+//  NCSNavField
+//
+//  Created by John Dzak on 8/31/12.
+//  Copyright (c) 2012 Northwestern University. All rights reserved.
+//
+
+//  Logic unit tests contain unit test code that is designed to be linked into an independent test executable.
+//  See Also: http://developer.apple.com/iphone/library/documentation/Xcode/Conceptual/iphone_development/135-Unit_Testing_Applications/unit_testing_applications.html
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "DatabaseTest.h"
+
+@interface NUResponseSetTest : DatabaseTest
+
+@end
