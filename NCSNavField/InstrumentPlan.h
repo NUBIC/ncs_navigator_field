fc4a2c6caca1440b16a1ab78276f9f1925c61ed4
John Dzak <j-dzak@northwestern.edu>
2012-09-07 11:31:02 -0500

    Add instrument plan model. #2418.

---
+++ NCSNavField/InstrumentPlan.h
@@ -0,0 +1,32 @@
+//
+//  InstrumentPlan.h
+//  NCSNavField
+//
+//  Created by Dzak, John J on 9/7/12.
+//  Copyright (c) 2012 Northwestern University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class InstrumentTemplate;
+
+@interface InstrumentPlan : NSManagedObject
+
+@property (nonatomic, retain) NSString * instrumentPlanId;
+@property (nonatomic, retain) NSOrderedSet *instrumentTemplates;
+@end
+
+@interface InstrumentPlan (CoreDataGeneratedAccessors)
+
+- (void)insertObject:(InstrumentTemplate *)value inInstrumentTemplatesAtIndex:(NSUInteger)idx;
+- (void)removeObjectFromInstrumentTemplatesAtIndex:(NSUInteger)idx;
+- (void)insertInstrumentTemplates:(NSArray *)value atIndexes:(NSIndexSet *)indexes;
+- (void)removeInstrumentTemplatesAtIndexes:(NSIndexSet *)indexes;
+- (void)replaceObjectInInstrumentTemplatesAtIndex:(NSUInteger)idx withObject:(InstrumentTemplate *)value;
+- (void)replaceInstrumentTemplatesAtIndexes:(NSIndexSet *)indexes withInstrumentTemplates:(NSArray *)values;
+- (void)addInstrumentTemplatesObject:(InstrumentTemplate *)value;
+- (void)removeInstrumentTemplatesObject:(InstrumentTemplate *)value;
+- (void)addInstrumentTemplates:(NSOrderedSet *)values;
+- (void)removeInstrumentTemplates:(NSOrderedSet *)values;
+@end
---
+++ NCSNavField/InstrumentPlan.m
@@ -0,0 +1,18 @@
+//
+//  InstrumentPlan.m
+//  NCSNavField
+//
+//  Created by Dzak, John J on 9/7/12.
+//  Copyright (c) 2012 Northwestern University. All rights reserved.
+//
+
+#import "InstrumentPlan.h"
+#import "InstrumentTemplate.h"
+
+
+@implementation InstrumentPlan
+
+@dynamic instrumentPlanId;
+@dynamic instrumentTemplates;
+
+@end
