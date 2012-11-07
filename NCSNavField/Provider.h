911320f2796708d3b00bdd74bf1d141a5d1cafef
John Dzak <john.dzak@gmail.com>
2012-11-07 10:15:12 -0600

    Add recruited attribute to Provider. #2637

--- NCSNavField/Provider.h
+++ NCSNavField/Provider.h
@@ -2,7 +2,7 @@
 //  Provider.h
 //  NCSNavField
 //
-//  Created by John Dzak on 11/5/12.
+//  Created by John Dzak on 11/7/12.
 //  Copyright (c) 2012 Northwestern University. All rights reserved.
 //
 
@@ -12,9 +12,10 @@
 
 @interface Provider : NSManagedObject
 
-@property (nonatomic, retain) NSString * providerId;
 @property (nonatomic, retain) NSNumber * locationNum;
 @property (nonatomic, retain) NSString * name;
 @property (nonatomic, retain) NSNumber * practiceNum;
+@property (nonatomic, retain) NSString * providerId;
+@property (nonatomic, retain) NSNumber * recruited;
 
 @end
--- NCSNavField/Provider.m
+++ NCSNavField/Provider.m
@@ -2,7 +2,7 @@
 //  Provider.m
 //  NCSNavField
 //
-//  Created by John Dzak on 11/5/12.
+//  Created by John Dzak on 11/7/12.
 //  Copyright (c) 2012 Northwestern University. All rights reserved.
 //
 
@@ -11,9 +11,10 @@
 
 @implementation Provider
 
-@dynamic providerId;
 @dynamic locationNum;
 @dynamic name;
 @dynamic practiceNum;
+@dynamic providerId;
+@dynamic recruited;
 
 @end
