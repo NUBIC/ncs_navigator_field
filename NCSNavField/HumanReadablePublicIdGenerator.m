3aa0a2205b67ddeb292e44595c73bc05717ed797
John Dzak <john.dzak@gmail.com>
2012-11-12 15:58:51 -0600

    Add test to check format of human readable public id generator. #2804

---
+++ NCSNavField/HumanReadablePublicIdGenerator.m
@@ -0,0 +1,17 @@
+//
+//  HumanReadablePublicIdGenerator.m
+//  NCSNavField
+//
+//  Created by John Dzak on 11/12/12.
+//  Copyright (c) 2012 Northwestern University. All rights reserved.
+//
+
+#import "HumanReadablePublicIdGenerator.h"
+
+@implementation HumanReadablePublicIdGenerator
+
++ (NSString*)createPublicId {
+    return @"abc-ab-abcd";
+}
+
+@end
---
+++ NCSNavFieldTests/HumanReadablePublicIdGeneratorTest.h
@@ -0,0 +1,13 @@
+//
+//  HumanReadablePublicIdGenerator.h
+//  NCSNavField
+//
+//  Created by John Dzak on 11/12/12.
+//  Copyright (c) 2012 Northwestern University. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HumanReadablePublicIdGeneratorTest : SenTestCase
+
+@end
