761d6c6f4d8a1188c94cea69258466009f6818c5
John Dzak <john.dzak@gmail.com>
2012-11-30 12:01:26 -0600

    Derive contacts, participants, and event templates on Fieldwork. #2936

--- NCSNavField/Fieldwork.h
+++ NCSNavField/Fieldwork.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2012 Northwestern University. All rights reserved.
 //
 
-#import <CoreData/CoreData.h>
-
 @class Contact, InstrumentTemplate, Participant;
 
 @interface Fieldwork : NSManagedObject
@@ -18,31 +16,6 @@
 
 @property(nonatomic,strong) NSDate* retrievedDate;
 
-@property(nonatomic,strong) NSSet* participants;
-
-@property(nonatomic,strong) NSSet* contacts;
-
-@property(nonatomic,strong) NSSet* instrumentTemplates;
-
 + (Fieldwork*)submission;
-
-@end
-
-@interface Fieldwork (CoreDataGeneratedAccessors)
-
-- (void)addContactsObject:(Contact *)value;
-- (void)removeContactsObject:(Contact *)value;
-- (void)addContacts:(NSSet *)values;
-- (void)removeContacts:(NSSet *)values;
-
-- (void)addInstrumentTemplatesObject:(InstrumentTemplate *)value;
-- (void)removeInstrumentTemplatesObject:(InstrumentTemplate *)value;
-- (void)addInstrumentTemplates:(NSSet *)values;
-- (void)removeInstrumentTemplates:(NSSet *)values;
-
-- (void)addParticipantsObject:(Participant *)value;
-- (void)removeParticipantsObject:(Participant *)value;
-- (void)addParticipants:(NSSet *)values;
-- (void)removeParticipants:(NSSet *)values;
 
 @end
