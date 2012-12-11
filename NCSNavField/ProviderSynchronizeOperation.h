f7c8e27d4f74b04d0e95ae2d1a1d2f855892be97
Samuel Hicks <samfhicks@gmail.com>
2012-12-11 15:46:14 -0600

    Making the error messages more human friendly

--- NCSNavField/ProviderSynchronizeOperation.h
+++ NCSNavField/ProviderSynchronizeOperation.h
@@ -8,16 +8,16 @@
 
 #import <Foundation/Foundation.h>
 
-#define CONTACT_RETRIEVAL @"We were trying to retrieve your contacts."
-#define PUTTING_DATA_ON_SERVER @"We were trying to put your fieldwork data on the server."
+#define CONTACT_RETRIEVAL @"We were trying to retrieve your data."
+#define PUTTING_DATA_ON_SERVER @"We were trying to put your data on the server."
 #define STORING_CONTACTS @"We were trying to store your contacts."
 #define MERGE_DATA @"We were trying to merge your data."
 #define PROVIDER_RETRIEVAL @"We were trying to retrieve providers."
-#define NCS_CODE_RETRIEVAL @"We were trying to get your ncs codes."
+#define NCS_CODE_RETRIEVAL @"We were trying to get your code lists."
 #define CAS_TICKET_RETRIEVAL @"We were trying to get you an authorization ticket."
 #define MERGE_IS_TAKING_TIME @"It is taking longer than expected."
 #define TRY_AGAIN_LATER @"Please try again later."
-#define MERGE_ERROR @"There was an error preventing you from obtaining your next contacts. Please call the help desk."
+#define MERGE_ERROR @"There was an error preventing you from obtaining your data. Please call the help desk."
 #define SYNCING_CONTACTS @"Syncing Contacts"
 
 @protocol UserErrorDelegate <NSObject>
