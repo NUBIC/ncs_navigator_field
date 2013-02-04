5811d4e60f64dcaf76d0bfefb8d863f179e2c3c6
John Dzak <john.dzak@gmail.com>
2013-02-04 11:52:50 -0600

    Remove old core data instrument plan relationship on instrument.

--- NCSNavField/RestKitSettings.m
+++ NCSNavField/RestKitSettings.m
@@ -188,8 +188,6 @@ static RestKitSettings* instance;
      @"supervisor_review_code", @"supervisorReviewId",
      @"data_problem_code", @"dataProblemId",
      @"instrument_comment", @"comment", nil];
-    [instrument mapRelationship:@"instrumentPlan" withMapping:instrumentPlan];
-    [instrument connectRelationship:@"instrumentPlan" withObjectForPrimaryKeyAttribute:@"instrumentPlanId"];
     
     // Event Mapping
     RKManagedObjectMapping* event = [RKManagedObjectMapping mappingForClass:[Event class] inManagedObjectStore:[RKObjectManager sharedManager].objectStore];
